854304b5463a8890868813e1103572f58323debd
sagar verma <eew.sagar@gmail.com>
2021-08-19 02:13:47 +0530

    delete java dir
    delete java dir

--- Java/Simple.java
+++
@@ -1,6 +0,0 @@
-class Simple{
-public static void main(String args[]){
- System.out.println("Hello Tech Rel , Campus is On....!!!");
-}
-  
-}
--- Java/sample1.java
+++
@@ -1,8 +0,0 @@
-class sample1{
-  public static void main(String [] args){
-   System.out.println("Hello World !!!");
- int a =10; int b =20;
-System.out.print("Sum of two number : "+(a+b));
-  }
-
-}
